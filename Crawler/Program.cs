9f3e6e2802ff5471125577fb78585abb762928de
Jon <jonathan.nut@gmail.com>
2019-04-12 12:13:13 +0100

    Testing more xpaths

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -10,6 +10,9 @@ namespace Crawler
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            Console.SetWindowSize(Console.LargestWindowWidth / 2, Console.LargestWindowHeight);
+            Console.SetWindowPosition(0, 0);
 
             var scraper = new Scraper.Scraper();
 
@@ -17,7 +20,8 @@ namespace Crawler
             PageLayout indexLayout = dotnetperls.AddPage("");
             PageLayout asyncLayout = dotnetperls.AddPage("async");
 
-            indexLayout.AddNode(new NodeRequest{Property = "Links", XPath = "//body//a"});
+            indexLayout.AddNode(new NodeRequest{Property = "Pages", XPath = "//body//a//b"});
+            indexLayout.AddNode(new NodeRequest{Property = "Description", XPath = "//body//a//div" });
             asyncLayout.AddNode(new NodeRequest{Property = "Title", XPath = "//body//a"});
 
             Console.WriteLine(dotnetperls.ToString());
