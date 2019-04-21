984ccd6575c56b7071367240dd6415ed22229b35
Snappey <jonathan.nut@gmail.com>
2019-04-21 20:05:13 +0100

    UI Framework

---
+++ Crawler/ConsoleManager.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crawler
+{
+    /// <summary>
+    /// Manages the UI Section of the Console Application
+    /// </summary>
+    class ConsoleManager
+    {
+
+        public ConsoleManager(int consoleWidth, int consoleHeight)
+        {
+            
+        }
+
+        private void Draw()
+        {
+            // Main Drawing Loop
+        }
+
+        public void Log()
+        {
+            // Outputs to main log in the centre screen
+            // Console.WriteLine etc. should be routed here
+        }
+    }
+}
