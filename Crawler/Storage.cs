3fe7a422a0c8fa483ed66627f840d65bbd0a8bb4
Snappey <jonathan.nut@gmail.com>
2019-04-21 21:45:42 +0100

    Added SQLite Driver

---
+++ Crawler/Storage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SQLite;
+
+namespace Crawler
+{
+    class Storage
+    {
+        private SQLiteConnection connection;
+        private SQLiteCommandBuilder queryBuilder;
+
+        public Storage(string database)
+        {
+            
+        }
+    }
+}
