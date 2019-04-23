8af5fea9f5b07f4c9733f5594374b8f7f0852818
Snappey <jonathan.nut@gmail.com>
2019-04-23 15:49:43 +0100

    Basic UI Stuff

--- Crawler/ConsoleManager.cs
+++ Crawler/ConsoleManager.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Crawler.UI;
 
 namespace Crawler
 {
@@ -10,10 +11,26 @@ namespace Crawler
     /// </summary>
     class ConsoleManager
     {
+        private Line[] lines;
+
 
         public ConsoleManager(int consoleWidth, int consoleHeight)
         {
-            
+            Console.BufferWidth = consoleWidth;
+            Console.BufferHeight = consoleHeight;
+
+            Console.Clear();
+            // Create an array of UI/Line.cs which represents each line of the console.
+            // States based on the command ran influences what is drawn(?)
+
+            Setup();
+        }
+
+        private void Setup()
+        {
+            lines = new Line[Console.BufferHeight];
+
+
         }
 
         private void Draw()
---
+++ Crawler/UI/Line.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crawler.UI
+{
+    /// <summary>
+    /// Each class represents a line on the console, one unit of information that  is redrawn each time as required. This is managed by ConsoleManager.cs based on the ConsoleHeight
+    /// </summary>
+    class Line
+    {
+        private string text;
+
+        public Line()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void SetText()
+        {
+
+        }
+    }
+}
