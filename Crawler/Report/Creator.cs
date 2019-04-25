8cb7dee4ab52451aba779dc64328066ef9dc35b4
Snappey <jonathan.nut@gmail.com>
2019-04-25 18:26:27 +0100

    Test HTML Generation

---
+++ Crawler/Report/Creator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using RazorLight;
+using Crawler.Report.Models;
+using Microsoft.CodeAnalysis.CSharp;
+
+
+namespace Crawler.Report
+{
+    class Creator
+    {
+        private string name;
+
+
+        public Creator(string name)
+        {
+            this.name = name;
+
+            Test();
+        }
+
+        private async void Test()
+        {
+            var engine = new RazorLightEngineBuilder()
+                .UseMemoryCachingProvider()
+                .UseFilesystemProject(Environment.CurrentDirectory + "/Report/Templates/")
+                .Build();
+
+            string template = "Testing, @Model.Name <br> This generated";
+            TestViewModel model = new TestViewModel
+            {
+                Name = name,
+            };
+
+            string result = await engine.CompileRenderAsync("Test.cshtml", model);
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Crawler/Report/Models/TestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crawler.Report.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+    }
+}
