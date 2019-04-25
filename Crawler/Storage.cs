a7a60999c63719377ea579a31d1ac0c317a15ce9
Snappey <jonathan.nut@gmail.com>
2019-04-25 18:26:47 +0100

    Implemented GetHotels

--- Crawler/Storage.cs
+++ Crawler/Storage.cs
@@ -75,6 +75,69 @@ namespace Crawler
 
         }
 
+        public void GetHotel()
+        {
+
+        }
+
+        public List<Hotel> GetHotels()
+        {
+            List<Hotel> hotels = new List<Hotel>();
+            using (SQLiteCommand command = new SQLiteCommand($"SELECT * FROM `hotels`", connection))
+            {
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Hotel hotel = new Hotel();
+
+                            hotel.Name = reader["name"] != null ? Convert.ToString(reader["name"]) : String.Empty;
+                            hotel.Address = reader["address"] != null ? Convert.ToString(reader["address"]) : String.Empty;
+                            hotel.City = reader["city"] != null ? Convert.ToString(reader["city"]) : String.Empty;
+                            hotel.Postcode = reader["postcode"] != null ? Convert.ToString(reader["postcode"]) : String.Empty;
+                            hotel.Phonenumber = reader["phonenumber"] != null ? Convert.ToString(reader["phonenumber"]) : String.Empty;
+                            hotel.Extras = reader["extras"] != null ? Convert.ToString(reader["extras"]) : String.Empty;
+                            hotel.ScrapeURL = reader["search_url"] != null ? Convert.ToString(reader["search_url"]) : String.Empty;
+                            hotel.HotelURL = reader["hotel_url"] != null ? Convert.ToString(reader["hotel_url"]) : String.Empty;
+
+                            if (hotel.ScrapeURL != null)
+                            {
+                                using (SQLiteCommand reserveCommand =
+                                    new SQLiteCommand("", connection))
+                                {
+                                    reserveCommand.CommandText = "SELECT * FROM `hotels_reservation` WHERE site=@site";
+                                    reserveCommand.Parameters.Add(new SQLiteParameter("site", hotel.ScrapeURL));
+
+                                    using (SQLiteDataReader reserveReader = reserveCommand.ExecuteReader())
+                                    {
+                                        if (reserveReader.HasRows)
+                                        {
+                                            while (reserveReader.Read())
+                                            {
+                                                HotelReservation reservation = new HotelReservation();
+
+                                                reservation.CheckOut = Convert.ToDateTime(reserveReader["checkout"]);
+                                                reservation.CheckIn = Convert.ToDateTime(reserveReader["checkin"]);
+                                                reservation.Price = reader["price"] != null ? Convert.ToString(reader["price"]) : String.Empty;
+                                                reservation.Currency = reader["currency"] != null ? Convert.ToString(reader["currency"]) : String.Empty;
+
+                                                hotel.ReservationData.AddDate(reservation);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            hotels.Add(hotel);
+                        }
+                    }
+                }
+            }
+
+            return hotels;
+        }
+
         private void Init()
         {
             connection = new SQLiteConnection();
